db9fafef2847ae87de851fd397045c31bf2eb180
JJQuary <e.semikov@gmail.com>
2021-05-26 21:39:05 +0300

    First attemps

---
+++ cmake-build-debug/CMakeFiles/Cursovaya2.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Calcifer/CLionProjects/Cursovaya2/Cursovaya/Joghurt.cpp" "C:/Users/Calcifer/CLionProjects/Cursovaya2/Cursovaya/cmake-build-debug/CMakeFiles/Cursovaya2.dir/Joghurt.cpp.obj"
+  "C:/Users/Calcifer/CLionProjects/Cursovaya2/Cursovaya/Product.cpp" "C:/Users/Calcifer/CLionProjects/Cursovaya2/Cursovaya/cmake-build-debug/CMakeFiles/Cursovaya2.dir/Product.cpp.obj"
+  "C:/Users/Calcifer/CLionProjects/Cursovaya2/Cursovaya/Shelf.cpp" "C:/Users/Calcifer/CLionProjects/Cursovaya2/Cursovaya/cmake-build-debug/CMakeFiles/Cursovaya2.dir/Shelf.cpp.obj"
+  "C:/Users/Calcifer/CLionProjects/Cursovaya2/Cursovaya/Tovar.cpp" "C:/Users/Calcifer/CLionProjects/Cursovaya2/Cursovaya/cmake-build-debug/CMakeFiles/Cursovaya2.dir/Tovar.cpp.obj"
+  "C:/Users/Calcifer/CLionProjects/Cursovaya2/Cursovaya/main.cpp" "C:/Users/Calcifer/CLionProjects/Cursovaya2/Cursovaya/cmake-build-debug/CMakeFiles/Cursovaya2.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Cursovaya2.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cursovaya2.dir/Joghurt.cpp.obj"
+  "CMakeFiles/Cursovaya2.dir/Product.cpp.obj"
+  "CMakeFiles/Cursovaya2.dir/Shelf.cpp.obj"
+  "CMakeFiles/Cursovaya2.dir/Tovar.cpp.obj"
+  "CMakeFiles/Cursovaya2.dir/main.cpp.obj"
+  "Cursovaya2.exe"
+  "Cursovaya2.exe.manifest"
+  "Cursovaya2.pdb"
+  "libCursovaya2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cursovaya2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
